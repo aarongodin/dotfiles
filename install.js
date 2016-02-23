bb5809ae5b48920ff5f0ca61d5b4097c1f9631f9
Aaron Godin <Aaron.Godin@bestbuy.com>
2016-02-23 13:49:02 -0600

    Install script

---
+++ install.js
@@ -0,0 +1,26 @@
+
+var fs = require('fs')
+
+var dotfiles = [
+	'.vimrc',
+	'.tmux.conf',
+	'.bash_profile'
+]
+
+dotfiles.forEach(function (file) {
+	var src = process.cwd() + "/" + file
+	var dest = process.env.HOME + "/" + file
+
+	fs.link(src, dest, function (err) {
+		if (err) {
+			if (err.errno === -17) {
+				return console.log("Already exists " + file)
+			}
+
+			return console.error("Failed to link" + file, err.message)
+		} else {
+			return console.log("Linked " + file)
+		}
+	})
+})
+
