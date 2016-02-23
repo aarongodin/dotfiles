d3086dde5f505fd713cd545f1ebfe5f8033b7b71
Aaron Godin <Aaron.Godin@bestbuy.com>
2016-02-23 13:50:19 -0600

    tabs to spaces

--- install.js
+++ install.js
@@ -2,25 +2,24 @@
 var fs = require('fs')
 
 var dotfiles = [
-	'.vimrc',
-	'.tmux.conf',
-	'.bash_profile'
+  '.vimrc',
+  '.tmux.conf',
+  '.bash_profile'
 ]
 
 dotfiles.forEach(function (file) {
-	var src = process.cwd() + "/" + file
-	var dest = process.env.HOME + "/" + file
+  var src = process.cwd() + "/" + file
+  var dest = process.env.HOME + "/" + file
 
-	fs.link(src, dest, function (err) {
-		if (err) {
-			if (err.errno === -17) {
-				return console.log("Already exists " + file)
-			}
+  fs.link(src, dest, function (err) {
+    if (err) {
+      if (err.errno === -17) {
+        return console.log("Already exists " + file)
+      }
 
-			return console.error("Failed to link" + file, err.message)
-		} else {
-			return console.log("Linked " + file)
-		}
-	})
+      return console.error("Failed to link" + file, err.message)
+    } else {
+      return console.log("Linked " + file)
+    }
+  })
 })
-
